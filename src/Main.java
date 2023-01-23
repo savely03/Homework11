0ed07acd649ebf87945af861434a686fb74e738c
Savely <savely.domnikov@yandex.ru>
2023-01-23 21:43:31 +0300

    Homework completed.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашнее задание 11");
+        System.out.println("Домашнее задание №11");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Task1:");
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        System.out.printf(" ФИО сотрудника - %s %s %s.\n", middleName, firstName, lastName);
+    }
+
+    public static void task2() {
+        System.out.println("Task2:");
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.printf(" Данные ФИО сотрудника для заполнения отчета - %s.\n", fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        System.out.println("Task3:");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'e' );
+        System.out.printf(" Данные ФИО сотрудника - %s.\n", fullName);
     }
 }
